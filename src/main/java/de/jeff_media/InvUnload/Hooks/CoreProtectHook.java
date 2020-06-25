cb2bb5172817ba5d7d11a5f9a1aa22f21ebfc81a
mfnalex <1122571+mfnalex@users.noreply.github.com>
2020-06-25 19:49:21 +0200

    4.3.0 release

--- src/main/java/de/jeff_media/InvUnload/Hooks/CoreProtectHook.java
+++ src/main/java/de/jeff_media/InvUnload/Hooks/CoreProtectHook.java
@@ -2,25 +2,34 @@ package de.jeff_media.InvUnload.Hooks;
 
 import de.jeff_media.InvUnload.Main;
 import net.coreprotect.CoreProtect;
+import org.bukkit.Location;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.Plugin;
 
 public class CoreProtectHook {
 
-    public static void logCoreProtect(Main main) {
+    Main main;
 
-        // TODO: This needs a CoreProtect APi update since its not possible to log what someone put into a chest with the current CP API
+    CoreProtectHook(Main main) {
+        this.main = main;
     }
 
-        /*Plugin coreProtectPlugin = main.getServer().getPluginManager().getPlugin("CoreProtect");
+    public static void logCoreProtect(Main main, String user, Inventory destination) {
+
+        // TODO: This needs a CoreProtect APi update since its not possible to log what someone put into a chest with the current CP API
+
+
+        Plugin coreProtectPlugin = main.getServer().getPluginManager().getPlugin("CoreProtect");
 
         if(coreProtectPlugin==null || !(coreProtectPlugin instanceof CoreProtect)) return;
 
+        Location location = destination.getLocation();
+
         CoreProtect cp = (CoreProtect) coreProtectPlugin;
 
-        cp.getAPI().logContainerTransaction(String user, Location location)
-                cp.getAPI().logPlacement(String user,Location loc, Material type, BlockData data)
+        cp.getAPI().logContainerTransaction(user, location);
 
 
-    }*/
+    }
 
 }
