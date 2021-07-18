8b6b738f3b8095a7610580bf8cc2e65d19ea4116
mfnalex <mail@jeff-media.de>
2021-07-18 11:19:40 +0200

    fixed enchantments for books

--- src/main/java/de/jeff_media/InvUnload/utils/EnchantmentUtils.java
+++ src/main/java/de/jeff_media/InvUnload/utils/EnchantmentUtils.java
@@ -3,8 +3,12 @@ package de.jeff_media.InvUnload.utils;
 import de.jeff_media.InvUnload.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Map;
 
 public class EnchantmentUtils {
 
@@ -34,6 +38,25 @@ public class EnchantmentUtils {
         //System.out.println(5);
         ItemMeta firstMeta = first.hasItemMeta() ? first.getItemMeta() : Bukkit.getItemFactory().getItemMeta(first.getType());
         ItemMeta secondMeta = second.hasItemMeta() ? second.getItemMeta() : Bukkit.getItemFactory().getItemMeta(second.getType());
+
+        if(firstMeta instanceof EnchantmentStorageMeta && secondMeta instanceof EnchantmentStorageMeta) {
+            EnchantmentStorageMeta firstStorage = (EnchantmentStorageMeta) firstMeta;
+            EnchantmentStorageMeta secondStorage = (EnchantmentStorageMeta) secondMeta;
+            if(firstStorage.getStoredEnchants().size() != secondStorage.getStoredEnchants().size()) {
+                return false;
+            }
+            for(Map.Entry<Enchantment,Integer> enchantment : firstStorage.getStoredEnchants().entrySet()) {
+                if(!secondStorage.getStoredEnchants().containsKey(enchantment.getKey())) {
+                    return false;
+                }
+                if(secondStorage.getStoredEnchantLevel(enchantment.getKey()) != enchantment.getValue()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
         //System.out.println(6);
         if(!firstMeta.hasEnchants() && !secondMeta.hasEnchants()) return true;
         //System.out.println(7);
